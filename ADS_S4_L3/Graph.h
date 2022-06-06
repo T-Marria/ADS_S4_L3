cd15c537a82265a07c23c9e89f311cdbf65512da
T-Marria <t.marria@mail.ru>
2022-06-06 23:34:30 +0400

    basic graph structures

--- ADS_S4_L3/Graph.h
+++ ADS_S4_L3/Graph.h
@@ -1,1 +1,25 @@
 #pragma once
+#include <list>
+#include <vector>
+using namespace std;
+
+template<typename TVertex, typename TEdge>
+class Graph {
+
+    struct Edge {
+        TEdge data;
+        TVertex dest;
+
+        Edge(const TEdge& data, const TVertex& destination) : data(data), dest(destination) {}
+
+    };
+
+    struct Vertex {
+        TVertex data;
+        list<Edge> _edges;
+
+        Vertex(const TVertex& data) : data(data) {}
+    };
+
+    vector<Vertex> table;
+};
